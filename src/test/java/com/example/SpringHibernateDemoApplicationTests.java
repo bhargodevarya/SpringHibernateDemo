6ccf53069449a3fd24d3be46592a9680810f9a15
Bhargodev Arya <bhaarya@publicisgroupe.net>
2018-11-26 14:00:16 +0530

    updating dependencies

--- src/test/java/com/example/SpringHibernateDemoApplicationTests.java
+++ src/test/java/com/example/SpringHibernateDemoApplicationTests.java
@@ -2,11 +2,12 @@ package com.example;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.SpringApplicationConfiguration;
+//import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = SpringHibernateDemoApplication.class)
+@ContextConfiguration(classes = SpringHibernateDemoApplication.class)
 public class SpringHibernateDemoApplicationTests {
 
 	@Test
